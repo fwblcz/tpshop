227b63c03891b24c5501084611a0164e2ac27118
houdunfuwenbin <1964855183@qq.com>
2019-10-31 15:03:36 +0800

    test admin

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\admin\controller;
+
+class Index
+{
+    public function index()
+    {
+        return '222';
+    }
+
+    public function test(){
+        echo 'test';
+    }
+}
