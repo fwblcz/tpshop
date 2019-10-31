0fda2ef619665edebb1110ae6c03f5fcbc44a740
houdunfuwenbin <1964855183@qq.com>
2019-10-31 16:21:37 +0800

    qqq

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\admin\controller;
+
+class Index
+{
+    public function index()
+    {
+        echo 'no';
+    }
+
+    public function test(){
+        echo '1111';exit;
+    }
+}
